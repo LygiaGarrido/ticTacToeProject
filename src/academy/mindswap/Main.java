9561d64dbe9aefd2bbe82efbf0b14a02ef0b4f1d
lygia <lygia.garrido@mindswap.academy>
2022-08-23 15:53:27 +0100

    single player feature working well -> created new methods in game to separate multiplayer start and singleplayer start; changed == to .equals() in check wins conditions

--- src/academy/mindswap/Main.java
+++ src/academy/mindswap/Main.java
@@ -1,10 +1,41 @@
 package academy.mindswap;
 
-import academy.mindswap.game.GameLogic;
+import academy.mindswap.game.*;
+import academy.mindswap.player.Player;
+import academy.mindswap.server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        GameLogic gl = new GameLogic();
-        gl.createBoard();
+
     }
+//    static String[][] board = {
+//            {" ", " ", " "},
+//            {"O", " ", "X"},
+//            {" ", "O", "X"}
+//    };
+//    public static void main(String[] args) {
+//
+//        //Antes
+//        System.out.println(drawBoard());
+//        fillBoard(SinglePlayer.botMovement(board));
+//        //Depois
+//        System.out.println(drawBoard());
+//
+//
+//    }
+//
+//    private static String drawBoard() {
+//        String bordDraw = "\n" + board[0][0] + "|" + board[0][1] + "|" + board[0][2] + "\n"
+//                + "______" + "\n" + board[1][0] + "|" + board[1][1] + "|" + board[1][2] + "\n" + "______"
+//                + "\n" + board[2][0] + "|" + board[2][1] + "|" + board[2][2] + "\n";
+//        return bordDraw;
+//    }
+//
+//    public static void fillBoard(int num){
+//        board[num / 3][num % 3] = "O";
+//    }
+
+
+
+
 }
